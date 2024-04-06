25839518c34056d448a05bbf3abfa75881e765e9
Trevi03 <trevi.yek21@imperial.ac.uk>
2024-04-06 12:59:56 +0100

    Tutorial Part 6

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -16,36 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-
-# Use include() to add paths from the catalog application
-from django.urls import include
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
-
-# Add URL maps to redirect the base URL to our application
+from django.urls import include  # Use include() to add paths from the catalog application
 from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]
-
-# Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-""" Can be rewritten in one go
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
- """
+    path('', RedirectView.as_view(url='catalog/')),   # Add URL maps to redirect the base URL to our application
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Use static() to add URL mapping to serve static files during development (only)
+
+
+# URL mapping
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
 
 
